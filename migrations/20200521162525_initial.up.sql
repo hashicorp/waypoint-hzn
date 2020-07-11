271ee0bbf712abc35772be87dd32ad4a9c8bbed2
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2020-07-11 14:20:54 -0700

    migraitons

---
+++ migrations/20200521162525_initial.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS registrations (
+  id SERIAL PRIMARY KEY,
+  account_id bytea NOT NULL UNIQUE,
+  email text NOT NULL,
+  name text
+);
+
+CREATE TABLE IF NOT EXISTS hostnames (
+  id SERIAL PRIMARY KEY,
+  registration_id int NOT NULL,
+  hostname text NOT NULL UNIQUE,
+  labels text[] NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT NOW(),
+  updated_at timestamptz NOT NULL DEFAULT NOW()
+);
